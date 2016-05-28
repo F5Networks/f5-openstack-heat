9054f554a0015d3bb05591e03846fc4c7c880165
Paul Breaux <p.breaux@f5.com>
2016-05-28 14:53:54 -0600

    Create clustering templates for active standby
    Issues:
    Fixes #76
    
    Problem:
    Two templates are needed, one to create clusterable VEs and one to
    actually cluster two devices.
    
    Analysis:
    Included here are the two templates for clustering and the functional
    tests. It is important to note that this will not merge until the
    clustering feature in f5-sdk is merged from development into 0.1.
    
    Tests:
    Two additional functional tests exist. One for the creation of two
    clusterable VEs and one for implementing the actual cluster.

---
+++ test/functional/ve/conftest.py
@@ -0,0 +1,44 @@
+# Copyright 2016 F5 Networks Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+from f5.bigip import BigIP
+
+import pytest
+import time
+
+
+@pytest.fixture
+def WaitForLicensedBigIP():
+    def poll_for_licensed_bigip(
+            bigip_ip, username, password, bigip_ver,
+            ifc_num, attempts=50, interval=10
+    ):
+        '''Poll the device until it is licensed and has a config.'''
+        bigip = BigIP(bigip_ip, username, password)
+        for attempt in range(attempts):
+            time.sleep(interval)
+            try:
+                registration = bigip.shared.licensing.registration.load()
+                assert registration.licensedVersion == bigip_ver
+                assert len(bigip.net.vlans.get_collection()) == ifc_num - 1
+                assert len(bigip.net.interfaces.get_collection()) == ifc_num
+                assert len(bigip.net.selfips.get_collection()) == ifc_num - 1
+                return bigip
+            except Exception as ex:
+                if attempt == (attempts - 1):
+                    raise ex
+                continue
+    return poll_for_licensed_bigip
